48755d100bfcaeef6712fa7e5d08132cb70abe16
bunnv2 <72439550+bunnv2@users.noreply.github.com>
2021-05-31 02:47:40 +0200

    added menu and font path

--- definitions.h
+++ definitions.h
@@ -1,13 +1,16 @@
 #pragma once
 /*
-	here all all the definitions we are using, f.e.
-		- texture paths
+	here are all the definitions we are using, f.e.
+		- file paths
 		- global variables (like screen width/height)
 		etc..
 
 */
 
 #define SCREEN_WIDTH 1200
-#define SCREEN_HEIGHT 800
+#define SCREEN_HEIGHT 900
 
-#define INIT_STATE_BACKGROUND_FILEPATH "res/bg.PNG"
+#define INIT_STATE_BACKGROUND_FILEPATH "res/initStateBackground.png"
+#define MENU_STATE_BACKGROUND_FILEPATH "res/menuStateBackground.png"
+
+#define PIXEL_FONT_FILEPATH "res/PixelBit.ttf"
