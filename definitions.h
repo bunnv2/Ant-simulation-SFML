07a3216b5b407ecefa5588c1515dc4391c277cd8
Alicja Gruca <72377506+glitterylungs@users.noreply.github.com>
2021-06-01 22:46:47 +0200

    Adding defines of rulesState.

--- definitions.h
+++ definitions.h
@@ -13,4 +13,12 @@
 #define INIT_STATE_BACKGROUND_FILEPATH "res/initStateBackground.png"
 #define MENU_STATE_BACKGROUND_FILEPATH "res/menuStateBackground.png"
 
+#define RULES_STATE_BACKGROUND_FILEPATH "res/rulesbackground.png"
+#define RULES_STATE_BUTTON_BACK_FILEPATH "res/rulesBackButton.png"
+#define RULES_STATE_NEST_FILEPATH "res/rulesNest.png"
+#define RULES_STATE_OBSTACLE_FILEPATH "res/rulesObstacle.png"
+#define RULES_STATE_FOOD_FILEPATH "res/rulesFood.png"
+#define RULES_STATE_ANTS_FILEPATH "res/rulesAnts.png"
+#define RULES_STATE_PHEROMONES_FILEPATH "res/rulesPheromones.png"
+
 #define PIXEL_FONT_FILEPATH "res/PixelBit.ttf"
