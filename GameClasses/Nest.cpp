2679f1aa642898e4560c2f899ac7c26aaeaa8d9a
bunnv2 <72439550+bunnv2@users.noreply.github.com>
2021-06-15 01:22:31 +0200

    ants spawn

--- GameClasses/Nest.cpp
+++ GameClasses/Nest.cpp
@@ -6,37 +6,20 @@ Nest::Nest(GameDataRef data) : _data(data)
 {
 }
 
-
-
-
-void Nest::drawNest() {
-
-	//for (unsigned int i = 0; i < _nestSprite.size(); i++)
-	//{
-		//_data->window.draw(_nestSprite.at(i));
-	//}
+void Nest::drawNest()
+{
 	_data->window.draw(_nestSprite);
-
 }
 
 void Nest::spawnNest(float x, float y)
 {
 	_nestSprite.setTexture(this->_data->assets.GetTexture("nestTexture"));
-	//sf::Sprite sprite(_data->assets.GetTexture("nest"));
-	//sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
-	//sprite.setPosition(x, y);
-
-	//_nestSprite.push_back(sprite);
-
 	_nestSprite.setOrigin(_nestSprite.getLocalBounds().width / 2, _nestSprite.getLocalBounds().height / 2);
 	_nestSprite.setPosition(x, y);
+
 }
 
-
-
-
-
-Nest::~Nest() {
-
+Nest::~Nest()
+{
 }
 
--- GameClasses/Nest.h
+++ GameClasses/Nest.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <vector>
+
 #include "../definitions.h"
 #include "../Engine/engine.h"
 
